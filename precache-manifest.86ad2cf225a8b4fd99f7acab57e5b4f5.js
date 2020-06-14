4f75f4b177a248259f6d41d7229dda5f58eaefe4
Alex <kleshnin.alexander@gmail.com>
2020-06-14 18:40:13 +0300

    update map

---
+++ precache-manifest.86ad2cf225a8b4fd99f7acab57e5b4f5.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9fa8b08f6bf964f9ad",
+    "url": "/css/app.e0e73bb2.css"
+  },
+  {
+    "revision": "fd2197e7b77d3a3e7a43",
+    "url": "/css/chunk-vendors.686dd7fc.css"
+  },
+  {
+    "revision": "8f326d0858a54d5fab35ef929dac42c1",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "7bb67099bb0d97163d08c9b92b685e42",
+    "url": "/img/mapillary_logo.7bb67099.png"
+  },
+  {
+    "revision": "de2944995ea6c57df82e521e70898e24",
+    "url": "/img/osm_logo.de294499.png"
+  },
+  {
+    "revision": "5b946ea50fc0ad5f6defc603b62b6398",
+    "url": "/img/pointer-wheat.5b946ea5.svg"
+  },
+  {
+    "revision": "e6734d100b29734992d543c5c16fa239",
+    "url": "/img/pointer-white.e6734d10.svg"
+  },
+  {
+    "revision": "020d7e85b2d34eac2e6c3baf925e20f8",
+    "url": "/img/scgis_logo.020d7e85.png"
+  },
+  {
+    "revision": "4365cd3c08542b32b14dc6f6eda99b46",
+    "url": "/img/stepper-left.4365cd3c.svg"
+  },
+  {
+    "revision": "f3bdcc97504bd826edbf2b2d1072d579",
+    "url": "/img/stepper-play.f3bdcc97.svg"
+  },
+  {
+    "revision": "bc856f8670be9c70177cc79d548dd54c",
+    "url": "/img/stepper-right.bc856f86.svg"
+  },
+  {
+    "revision": "8e3cdb421bdba568c22ef0e0555faf55",
+    "url": "/img/stepper-stop.8e3cdb42.svg"
+  },
+  {
+    "revision": "d825567a1a1968dea71c7424e74f2996",
+    "url": "/img/turn-around.d825567a.svg"
+  },
+  {
+    "revision": "48f442bffb936063caebc146efa367fa",
+    "url": "/img/turn.48f442bf.svg"
+  },
+  {
+    "revision": "df2776dddbe690645b739cc6426464a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd9fa8b08f6bf964f9ad",
+    "url": "/js/app.d077401c.js"
+  },
+  {
+    "revision": "fd2197e7b77d3a3e7a43",
+    "url": "/js/chunk-vendors.d7ee7ef7.js"
+  },
+  {
+    "revision": "1a0345feecbac7d8b8a0",
+    "url": "/js/notfound.2d8c00ec.js"
+  },
+  {
+    "revision": "56a0ad51ea2e2d3909b28b22915fd7bf",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b729228a2a812782c7ccd696968f09eb",
+    "url": "/page.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
